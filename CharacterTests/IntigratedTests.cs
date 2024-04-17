ecc02e133ed065a2b6380dd731a05251905251e0
Derpipose <thefluffmaster2000@gmail.com>
2024-04-17 15:50:19 -0600

    Got Testing working

---
+++ CharacterTests/IntigratedTests.cs
@@ -0,0 +1,96 @@
+﻿using CharacterBuilderShared.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace CharacterTests
+{
+    public class Tests
+    {
+
+        PlayerService playerService;
+        [SetUp]
+        public void Setup()
+        {
+
+            string connection = "host=test-my_postgres_db:5432;Database=mydatabase;Username=myusername;Password=mypassword;";
+            DbContextOptions<BuilderContext> options = new DbContextOptionsBuilder<BuilderContext>()
+            .UseNpgsql(connection)
+            .Options;
+            var loggerFactory = new LoggerFactory();
+
+            var characterlogger = loggerFactory.CreateLogger<Player>();
+
+            BuilderContext characterdbContext = new BuilderContext(options);
+            playerService = new(characterlogger, characterdbContext);
+
+        }
+
+        [Test]
+        public void PlayerCreate()
+        {
+            Player player = new Player
+            {
+                Pin = 1234,
+                PlayerName = "Hilton",
+                Username = "Hamilioton"
+            };
+            Assert.That(player, Is.Not.Null);
+            Assert.That(player.PlayerName, Is.EqualTo("Hilton"));
+            Assert.That(player.Username, Is.EqualTo("Hamilioton"));
+            Assert.That(player.Pin, Is.EqualTo(1234));
+        }
+
+        [Test]
+        public async Task PlayerAdd()
+        {
+            Player player = new Player
+            {
+                Pin = 1234,
+                PlayerName = "Hilton",
+                Username = "Hamilioton"
+            };
+            Assert.That(player, Is.Not.Null);
+            Assert.That(player.PlayerName, Is.EqualTo("Hilton"));
+            Assert.That(player.Username, Is.EqualTo("Hamilioton"));
+            Assert.That(player.Pin, Is.EqualTo(1234));
+            await playerService.AddPlayer(player);
+            var mylist = await playerService.GetAllPlayers();
+            List<Player> list = mylist.ToList();
+            Assert.That(list.Count, Is.EqualTo(1));
+            Assert.That(list[0].PlayerName, Is.EqualTo("Hilton"));
+            Assert.That(list[0].Username, Is.EqualTo("Hamilioton"));
+            Assert.That(list[0].Pin, Is.EqualTo(1234));
+
+        }
+
+        [Test]
+        public async Task PlayerModify()
+        {
+            
+            var mylist = await playerService.GetAllPlayers();
+            List<Player> list = mylist.ToList();
+            Assert.That(list.Count, Is.EqualTo(1));
+            Assert.That(list[0].PlayerName, Is.EqualTo("Hilton"));
+            Assert.That(list[0].Username, Is.EqualTo("Hamilioton"));
+            Assert.That(list[0].Pin, Is.EqualTo(1234));
+            Player updateplayer = new Player
+            {
+                Id = 1,
+                Username = "Test",
+                PlayerName = "Hamilton",
+                Pin = 4321
+            };
+            await playerService.UpdatePlayer(updateplayer);
+            Assert.That(list.Count, Is.EqualTo(1));
+            Assert.That(list[0].PlayerName, Is.EqualTo("Hamilton"));
+            Assert.That(list[0].Username, Is.EqualTo("Hamilioton"));
+            Assert.That(list[0].Pin, Is.EqualTo(4321));
+        }
+
+
+
+
+
+
+    }
+}
--- README.md
+++ README.md
@@ -16,6 +16,7 @@ API: Almost done- should be done tonight.
 Swagger: Should be up and running by tonight
 Front end: 
 Undone and untouched. Will be started next week, as early as sunday. 
+
 Telemetry:
 Dashboards- not started as there is nothing yet to be started with. 
 Operations:
@@ -30,5 +31,6 @@ SSL and DNS- Not quite there. 
 Auto backups- working on that and should have it when the rest of the app is up. 
 Auto restore- Again not quite sure what this is asking for but I should be able to accomplish this with the Kube!
 Feature Flags- Not sure but we haven't really covered any of this. 
+
 K8
 None of this has been attempted as we haven't done any of this in class. 
