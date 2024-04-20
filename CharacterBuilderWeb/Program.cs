0f6c79819bfeb9b1dd5b690688dbb572c09a582a
Derpipose <thefluffmaster2000@gmail.com>
2024-04-19 20:09:36 -0600

    Got the create user page up. Not sure if it is working yet.

--- CharacterBuilderWeb/Program.cs
+++ CharacterBuilderWeb/Program.cs
@@ -1,21 +1,59 @@
 ﻿using CharacterBuilderWeb.Components;
+using CharacterBuilderWeb.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 // string uri = "http://thederpeningapiimage:8080";
 string uri = Environment.GetEnvironmentVariable("apiaccess") ?? "http://characterbuilderweb-api:8080";
-// Add services to the container.
-builder.Services.AddRazorPages();
 
-builder.Services.AddScoped<ApiService>(provider =>
+builder.Services.AddScoped<CharacterApiService>(provider =>
 {
     HttpClient httpclient = new HttpClient { BaseAddress = new Uri(uri) };
-    return new ApiService(httpclient);
+    return new CharacterApiService(httpclient);
 });
 
+builder.Services.AddScoped<CharClassApiService>(provider =>
+{
+    HttpClient httpclient = new HttpClient { BaseAddress = new Uri(uri) };
+    return new CharClassApiService(httpclient);
+});
 
+builder.Services.AddScoped<CharRaceApiService>(provider =>
+{
+    HttpClient httpclient = new HttpClient { BaseAddress = new Uri(uri) };
+    return new CharRaceApiService(httpclient);
+});
+
+builder.Services.AddScoped<ModStatsApiService>(provider =>
+{
+    HttpClient httpclient = new HttpClient { BaseAddress = new Uri(uri) };
+    return new ModStatsApiService(httpclient);
+});
+
+builder.Services.AddScoped<PlayerApiService>(provider =>
+{
+    HttpClient httpclient = new HttpClient { BaseAddress = new Uri(uri) };
+    return new PlayerApiService(httpclient);
+});
+
+builder.Services.AddScoped<RaceVarApiService>(provider =>
+{
+    HttpClient httpclient = new HttpClient { BaseAddress = new Uri(uri) };
+    return new RaceVarApiService(httpclient);
+});
+
+builder.Services.AddScoped<StatsApiService>(provider =>
+{
+    HttpClient httpclient = new HttpClient { BaseAddress = new Uri(uri) };
+    return new StatsApiService(httpclient);
+});
 
 builder.Services.AddServerSideBlazor();
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 
+
+
+// Add services to the container.
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
